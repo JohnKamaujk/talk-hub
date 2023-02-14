ba73cb78cd2c206988ac4fec7a01316402500b01
JohnKamaujk <johnjkkamau@gmail.com>
2023-02-14 12:08:07 +0300

    :sparkles:feat:Application routes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
-function App() {
+import { Route, Routes, Navigate } from "react-router-dom";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+const App = () => {
   return (
-    <div>
-      <p>Chat App</p>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Chat = () => {
+  return <>Chat</>;
+};
+
+export default Chat;
