1f21a2e3e58bcc5b48addfacca652c39ac9b4899
JohnKamaujk <johnjkkamau@gmail.com>
2023-02-19 12:19:48 +0300

    :sparkles:feat:Chat list component added

--- client/src/components/chat.jsx/UserChat.jsx
+++ client/src/components/chat.jsx/UserChat.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Stack } from "react-bootstrap";
+import { useFetchRecipientUser } from "../../hooks/useFetchRecipient";
+import avatar from "../../assets/avatar.svg";
 
-const UserChat = ({chat,user}) => {
+const UserChat = ({ chat, user }) => {
+  const { recipientUser } = useFetchRecipientUser(chat, user);
+
+  console.log(recipientUser);
+
   return (
-    <div>UserChat</div>
-  )
-}
+    <Stack
+      direction="horizontal"
+      gap={3}
+      role="button"
+      className="user-card align-items-center justify-content-between p-2 "
+    >
+      <div className="d-flex">
+        <div className="me-2">
+          <img src={avatar} height="35px" />
+        </div>
+        <div className="text-content">
+          <div className="name">{recipientUser?.name}</div>
+          <div className="text">text message</div>
+        </div>
+      </div>
+      <div className="d-flex flex-column align-items-end">
+        <div className="date">19/02/2023</div>
+        <div className="this-user-notifications">2</div>
+        <span className="user-online"></span>
+      </div>
+    </Stack>
+  );
+};
 
-export default UserChat
+export default UserChat;
