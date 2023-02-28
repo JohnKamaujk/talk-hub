ce3acbb22ed40a32fc62180ec04b12dc557a36d2
JohnKamaujk <johnjkkamau@gmail.com>
2023-02-28 14:53:17 +0300

    :sparkles:feat:toggle div onlick for notification component

--- client/src/components/chat.jsx/Notification.jsx
+++ client/src/components/chat.jsx/Notification.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Notification = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="notifications">
-      <div className="notifications-icon">
+      <div className="notifications-icon" onClick={() => setIsOpen(!isOpen)}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="20"
@@ -15,6 +17,14 @@ const Notification = () => {
           <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
         </svg>
       </div>
+      {isOpen && (
+        <div className="notifications-box">
+          <div className="notifications-header">
+            <h3>Notifications</h3>
+            <div className="mark-as-read">Mark all as read</div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
